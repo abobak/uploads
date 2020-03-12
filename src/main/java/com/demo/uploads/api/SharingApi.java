88c300d1f40616690b77ed15ee33d358aaf6b73d
Andrzej Bobak <a.bobak@interia.pl>
2020-03-12 21:28:11 +0100

    Provide swagger documentation for sharing action

--- src/main/java/com/demo/uploads/api/SharingApi.java
+++ src/main/java/com/demo/uploads/api/SharingApi.java
@@ -1,16 +1,16 @@
 package com.demo.uploads.api;
 
-import com.demo.uploads.dto.FileShareDto;
+import com.demo.uploads.dto.FileSharesDto;
+import com.demo.uploads.dto.SharePayload;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import com.demo.uploads.dto.SharePayload;
-import com.demo.uploads.dto.FileSharesDto;
 
 @Api(value = "sharing", tags = { "sharing" })
 public interface SharingApi {
 
     @ApiOperation(value = "Share file with given identifier to user with given email")
-    void share(FileShareDto dto);
+    void share(SharePayload dto);
 
+    @ApiOperation(value = "Show files shared with authenticated user")
     FileSharesDto showAvailableShares();
 }
