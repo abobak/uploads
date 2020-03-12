633eb9a00a33c4bef1ebb58d92dc339e373fccfc
Andrzej Bobak <a.bobak@interia.pl>
2020-03-12 23:42:20 +0100

    Add sharing controller tests

--- src/main/java/com/demo/uploads/controller/SharingController.java
+++ src/main/java/com/demo/uploads/controller/SharingController.java
@@ -1,7 +1,6 @@
 package com.demo.uploads.controller;
 
 import com.demo.uploads.api.SharingApi;
-import com.demo.uploads.dto.FileShareDto;
 import com.demo.uploads.dto.SharePayload;
 import com.demo.uploads.dto.FileSharesDto;
 import com.demo.uploads.model.User;
@@ -10,7 +9,10 @@ import com.demo.uploads.service.FilesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +23,7 @@ public class SharingController implements SharingApi {
     private final FilesService filesService;
 
     @PostMapping("/api/share")
-    public void share(SharePayload dto) {
+    public void share(@Valid @RequestBody SharePayload dto) {
         User currentUser = userSecurityHelper.getCurrentUser();
         filesService.shareWithOtherUser(dto.getShareIdentifier(), dto.getSharedWith(), currentUser);
     }
--- src/main/java/com/demo/uploads/dto/SharePayload.java
+++ src/main/java/com/demo/uploads/dto/SharePayload.java
@@ -2,14 +2,18 @@ package com.demo.uploads.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class SharePayload {
 
+    @NotNull
     private String sharedWith;
 
+    @NotNull
     private String shareIdentifier;
 
 }
